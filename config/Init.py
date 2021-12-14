2d327d85e90b32377e9099152c425639d21e1517
Logan <lbendtlynielsen@gmail.com>
2021-12-13 21:30:55 -0700

    green cubes

--- config/Init.py
+++ config/Init.py
@@ -11,7 +11,7 @@ objects = [
     Sphere( np.array([ 0.25, -2, 12.5]), 1, "BLUE", "WHITE", 0.2, 0.00, "air", 1, 1),
     Sphere( np.array([-2, -2, 11]), 1, "RED", "WHITE", 0.2, 0.00, "air", 1, 1),
     # Sphere( np.array([ 2, -2, 9]), 1, "GREEN", "WHITE", 0.2, 0.00, "air", 1, 1),
-    Cube(1, 3, -3, -1, 8, 10, "GREEN", "WHITE", 0.2, 0.00, "air", 1, 1),
+    Cube(1.5, 3, -3, -1.5, 8.5, 10, "GREEN", "WHITE", 0.2, 0.00, "air", 1, 1),
     # Cube(-3,-1,-1,1,13,15,"DARK_GRAY","WHITE",.1,0, None,1,1),
     # Sphere( np.array([ -1.75, -2, 9]), 1, "WHITE", "WHITE", 0.1, .95, "air", 1, 1),
     # Sphere( np.array([ 2, -2, 8]), 1, "WHITE", "WHITE", 0.1, .95, "air", 1, 1),
@@ -27,16 +27,16 @@ objects = [
 # light sources (does not include ambient lighting)
 # see phong illumination for that
 directional_lights = [
-    DirectionalLightSource( np.array([0.5,-1,1]), "WHITE", 1),
+    # DirectionalLightSource( np.array([0.5,-1,1]), "WHITE", 1),
     # DirectionalLightSource( np.array([.5,-2,-1]), "WHITE", 1),
     # DirectionalLightSource( np.array([.5,-.1,1]), "WHITE", 1)
 ]
 
 area_lights = [
-    # AreaLightSource([
-    #     np.array([-3, 1, 4]),
-    #     np.array([-1, 1, 4]),
-    #     np.array([-1, 1, 2]),
-    #     np.array([-3, 1, 2]),
-    # ], "WHITE", 1)
+    AreaLightSource([
+        np.array([-3, 1, 4]),
+        np.array([-1, 1, 4]),
+        np.array([-1, 1, 2]),
+        np.array([-3, 1, 2]),
+    ], "WHITE", 1)
 ]
